cffd511bc558daef95520f35e158aae57d2d35d3
Daniel Ralfes Silva de Abreu <danielralfes@gmail.com>
2024-11-29 12:06:37 -0300

    Add project files.

---
+++ Cliente/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poli
+{
+    public class Animal
+    {
+        public virtual void EmitirSom() { Console.WriteLine("Som genérico"); }
+    }
+
+    public class Cao : Animal
+    {
+        public override void EmitirSom() { Console.WriteLine("Au au"); }
+    }
+
+    public class Gato : Animal
+    {
+        public override void EmitirSom() { Console.WriteLine("Miau"); }
+    }
+
+    public class ExecutaSons
+    {
+        public void Executar()
+        {
+            var animais = new List<Animal> { new Cao(), new Gato() };
+
+            foreach (var animalSom in animais)
+            {
+                animalSom.EmitirSom();
+            }
+        }
+    }
+}
---
+++ Cliente/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System.Net.WebSockets;
+using System.Text;
+
+using var ws = new ClientWebSocket();
+await ws.ConnectAsync(new Uri("ws://localhost:5136/"), CancellationToken.None);
+
+var buffer = new byte[256];
+while (ws.State == WebSocketState.Open)
+{
+    var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+    if (result.MessageType == WebSocketMessageType.Close)
+        await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+    else
+        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, result.Count));
+}
